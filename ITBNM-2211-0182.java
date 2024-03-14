3cfa5a32203253e89b64c35ee88e652263829f39
ckrcts1 <163437682+ckrcts1@users.noreply.github.com>
2024-03-14 22:02:10 +0530

    Add files via upload

---
+++ ITBNM-2211-0182.java
@@ -0,0 +1,26 @@
+public class Inky{
+
+public Inky(String x) {
+System.out.println(x);
+}
+
+public Inky (String pinky, String blinky, String rinky) {
+System.out.println(blinky + " " + pinky);
+System.out.println(pinky + " " + rinky);
+System.out.println(blinky + " " + pinky + " " + rinky); 
+}
+
+public static void main (String args[]) {
+
+String pinky = "Donkey";
+
+Inky ponky =new Inkey (pinky);
+
+String blinky = "Pinky";
+String rinky = "Monky";
+
+Inky ponky2 = new Inky(pinky, blinky, rinky):
+
+}
+
+}
